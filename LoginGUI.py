b2893e4065c5da8a391367cd7ecdf52c6fc7fbda
Steven Lau <stev.lau@mail.utoronto.ca>
2021-11-06 16:48:11 -0400

    added basic window

--- LoginGUI.py
+++ LoginGUI.py
@@ -1,3 +1,15 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class LoginScreen(App):
+    def build(self):
+        self.window = GridLayout()
+
 def request_login():
     return None
 
